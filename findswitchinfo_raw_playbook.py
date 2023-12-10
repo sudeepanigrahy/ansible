dcb7b3f82a833789b347bf1ad4f15c216c8c168e
sudeepanigrahy <115879989+sudeepanigrahy@users.noreply.github.com>
2023-12-10 11:39:32 +0530

    Update findswitchinfo_raw_playbook.py

--- findswitchinfo_raw_playbook.py
+++ findswitchinfo_raw_playbook.py
@@ -1,8 +1,8 @@
 # The python script will have to be executed from cli, and
 # the switch and cli_command will have to be passed, in that order,
 # inside of double-quotes, from the cli itself, two examples are:
-# >python .\findswitchinfo_raw_playbook.py "sg209-b1rm5x-as6-cn240.sing.micron.com" "sh ip int br" "| additional grepping if you'd like"
-# >python .\findswitchinfo_raw_playbook.py "sg209-b1rm5x-as6-cn240.sing.micron.com" "get mac address-table" "| grep -v 'CPU\|Gi1/1/1'"
+# >python .\findswitchinfo_raw_playbook.py "****************" "sh ip int br" "| additional grepping if you'd like"
+# >python .\findswitchinfo_raw_playbook.py "******************" "get mac address-table" "| grep -v 'CPU\|Gi1/1/1'"
 import sys
 import time
 from netmiko import ConnectHandler
@@ -25,15 +25,15 @@ def switch_info_finder(connection, switch, command, grepping):
     #time.sleep(100/1000)
 
     if command=="sh ip int br":
-        ansible_command = f"ansible-playbook -i /root/hosts1 shipintbr_raw.yml -u spanigrahy -k  {grepping}"
+        ansible_command = f"ansible-playbook -i /root/hosts1 shipintbr_raw.yml -u ********** -k  {grepping}"
     elif command=="sh ver" or command=="sh version":
-        ansible_command = f"ansible-playbook -i /root/hosts1 shver_raw.yml -u spanigrahy -k  {grepping}"
+        ansible_command = f"ansible-playbook -i /root/hosts1 shver_raw.yml -u ********** -k  {grepping}"
     elif command=="sh int status":
-        ansible_command = f"ansible-playbook -i /root/hosts1 shintstatus_raw.yml -u spanigrahy -k  {grepping}"
+        ansible_command = f"ansible-playbook -i /root/hosts1 shintstatus_raw.yml -u ********** -k  {grepping}"
     elif 'mac' in command or 'address-table' in command:
-        ansible_command = f"ansible-playbook -i /root/hosts1 getmac_raw.yml -u spanigrahy -k  {grepping}"
+        ansible_command = f"ansible-playbook -i /root/hosts1 getmac_raw.yml -u ********** -k  {grepping}"
     elif 'cdp' in command or 'nei' in command or 'neighbors' in command or 'neighbor' in command:
-        ansible_command = f"ansible-playbook -i /root/hosts1 shcdpnei_raw.yml -u spanigrahy -k  {grepping}"
+        ansible_command = f"ansible-playbook -i /root/hosts1 shcdpnei_raw.yml -u ********** -k  {grepping}"
     else: print("Type the command clearly..")
 
     print(f"\nExecuting on Ansible: {ansible_command}\n")
@@ -61,9 +61,9 @@ if __name__=="__main__":
 
     kwargs = {
         'device_type': 'linux',
-        'ip': '10.70.85.211',
-        'username': 'spanigrahy',
-        'password': 'Sueme@0128',
+        'ip': '10.x.x.x',
+        'username': '**********',
+        'password': '**********',
         'port': 22,
         'verbose':True
         }
